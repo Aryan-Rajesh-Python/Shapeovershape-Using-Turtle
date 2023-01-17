87b8b8d90d125a5828e3b164c35210ecc6437300
Aryan Rajesh <122169573+Aryan-Rajesh-Python@users.noreply.github.com>
2023-01-17 14:31:42 +0530

    Add files via upload

--- shapeovershape.py
+++ shapeovershape.py
@@ -4,12 +4,20 @@ from turtle import Turtle,Screen
 tortoise=Turtle()
 screen=Screen()
 colors=["red","green","blue","brown","pink","violet","yellow"]
-def draw_shape(num_sides):
-    a=(360/num_sides)
-    for i in range(num_sides):
-        tortoise.forward(100)
-        tortoise.left(a)
-for i in range(3,15):
-    tortoise.color(random.choice(colors))
-    draw_shape(i)
-screen.exitonclick()
+def shapeovershape():
+    def draw_shape(num_sides):
+        a=(360/num_sides)
+        for i in range(num_sides):
+            tortoise.forward(100)
+            tortoise.left(a)
+    for i in range(3,15):
+        tortoise.color(random.choice(colors))
+        draw_shape(i)
+    screen.exitonclick()
+while(True):
+    choice=input("Do you want to continue drawing shapeovershape: ")
+    if(choice=="yes"):
+        shapeovershape()
+    else:
+        print("Thank you for using our application!")
+        break
